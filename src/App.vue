5c4e9486d999d95aec25acb8100ac9e27f6fa00a
upupzealot <hzxjhspxn@163.com>
2025-01-11 22:42:38 +0800

    UI framework

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,52 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="container">
+    <div class="layout">
+      <div class="header">
+        <div></div>
+      </div>
+      <div class="body">
+        <RouterView />
+      </div>
+      <div class="footer"></div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.container {
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
 }
+.layout {
+  width: 1000px;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+  justify-content: flex-start;
+  align-items: stretch;
+
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.header,
+.footer {
+  flex-grow: 0;
+  min-height: 46px;
+  background-color: lightgrey;
 }
+.body {
+  flex-grow: 1;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  display: flex;
+  align-items: stretch;
+  flex-direction: row;
+  justify-content: center;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-  </main>
+<template class="wrapper">
+  <div class="side-bar">Left</div>
+  <div class="center"></div>
+  <div class="side-bar">Right</div>
 </template>
+
+<style scoped>
+.side-bar {
+  width: 200px;
+  flex-grow: 0;
+  background-color: aliceblue;
+}
+.center {
+  flex-grow: 1;
+  max-width: 600px;
+  background-color: white;
+}
+</style>
+
+<script setup lang="ts"></script>
