6f0dcd2a5194c154173e3436a8a804f2f22bfe5f
upupzealot <hzxjhspxn@163.com>
2025-01-17 16:31:33 +0800

    update: 增加项目，条纹 T

--- src/assets/item-data/shirt.ts
+++ src/assets/item-data/shirt.ts
@@ -1,5 +1,7 @@
 import none from '@/assets/item-data/none.json'
 
 import shirt1 from '@/../public/images/shirt/shirt-1.json'
+import shirt2 from '@/../public/images/shirt/shirt-2.json'
+import shirt3 from '@/../public/images/shirt/shirt-3.json'
 
-export default [none, shirt1]
+export default [none, shirt1, shirt2, shirt3]
