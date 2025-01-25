2f22473999853eacd9f7afd0cc039ae4f217f0a7
upupzealot <hzxjhspxn@163.com>
2025-01-25 21:13:06 +0800

    update: 增加武器-长矛

--- src/assets/item-data/weapon.ts
+++ src/assets/item-data/weapon.ts
@@ -1,5 +1,6 @@
 import none from '@/assets/item-data/none.json'
 
 import weapon1 from '@/../public/images/weapon/weapon-1.json'
+import weapon2 from '@/../public/images/weapon/weapon-2.json'
 
-export default [none, weapon1]
+export default [none, weapon1, weapon2]
