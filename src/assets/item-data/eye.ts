7589b558a71b2ddf1285620bbda3873ff65098c8
upupzealot <hzxjhspxn@163.com>
2025-02-09 14:32:47 +0800

    update: 增加眼睛图层

--- src/assets/item-data/eye.ts
+++ src/assets/item-data/eye.ts
@@ -3,5 +3,6 @@ import none from '@/assets/item-data/none.json'
 import eye1 from '@/../public/images/eye/eye-1.json'
 import eye2 from '@/../public/images/eye/eye-2.json'
 import eye3 from '@/../public/images/eye/eye-3.json'
+import eye4 from '@/../public/images/eye/eye-4.json'
 
-export default [none, eye1, eye2, eye3]
+export default [none, eye1, eye2, eye3, eye4]
