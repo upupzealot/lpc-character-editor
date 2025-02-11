03e952501b161a64707bc42b626f5ebbe5c41441
upupzealot <hzxjhspxn@163.com>
2025-02-11 16:30:13 +0800

    update: +2 发型部件

--- src/assets/item-data/hair.ts
+++ src/assets/item-data/hair.ts
@@ -4,5 +4,7 @@ import hair1 from '@/../public/images/hair/hair-1.json'
 import hair2 from '@/../public/images/hair/hair-2.json'
 import hair3 from '@/../public/images/hair/hair-3.json'
 import hair4 from '@/../public/images/hair/hair-4.json'
+import hair5 from '@/../public/images/hair/hair-5.json'
+import hair6 from '@/../public/images/hair/hair-6.json'
 
-export default [none, hair1, hair2, hair3, hair4]
+export default [none, hair1, hair2, hair3, hair4, hair5, hair6]
